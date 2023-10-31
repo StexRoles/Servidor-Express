1f2ac4d67e69bab55e354bd5f14d41e39b41eb65
StexRoles <navarro456123@gmail.com>
2023-10-31 12:15:47 -0500

    Uniendo las paginas con el servidor

--- app.js
+++ app.js
@@ -4,36 +4,36 @@ const path = require('path');
 
 app.listen(3030, () => {
     console.log('Servidor en ejecucion');
-});s
+});
 
 app.get('/', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/index.html'));
 });
 
 app.get('/babbage', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/babbage.html'));
 });
 
 app.get('/berners-lee', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/berners-lee.html'));
 });
 
 app.get('/clarke', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/clarke.html'));
 });
 
 app.get('/hamilton', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/hamilton.html'));
 });
 
 app.get('/hopper', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/hopper.html'));
 });
 
 app.get('/lovelace', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/lovelace.html'));
 });
 
 app.get('/turing', function(req, res){
-    res.sendFile(path.resolve(__dirname, './gatito.jpg'));
+    res.sendFile(path.resolve(__dirname, './DH-HEROES/views/turing.html'));
 });
